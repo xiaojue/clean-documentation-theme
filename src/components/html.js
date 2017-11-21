e53104ce3dd180e4002d50f45ab9afc79de9aaaf
xiaojue <designsor@gmail.com>
2017-11-21 15:37:34 +0800

    change google font -> fonts cat

--- src/components/html.js
+++ src/components/html.js
@@ -27,7 +27,7 @@ module.exports = ({name, content}) => {
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>{name} - Documentation</title>
-        <link href='https://fonts.googleapis.com/css?family=Roboto+Mono:400,500|Roboto:300,500,700' rel='stylesheet' />
+        <link href='https://fonts.cat.net/css?family=Roboto+Mono:400,500|Roboto:300,500,700' rel='stylesheet' />
         <Style rules={bodyStyle} />
       </head>
       <body>
